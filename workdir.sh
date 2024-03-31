1fdebc3cb2b838c1343d6198fe781904f73625a4
Ahmed Khatab <khatabah@rsws19.kaust.edu.sa>
2024-03-31 05:17:28 +0300

    script to set working directory

---
+++ workdir.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo -e "Please enter your working directory: \n"
+
+read workdir
+
+MATLABPATH="$HOME/IMAT"
+
+sed -i '250s|.*|ljsl = ['\'''$workdir/Jobs/''\'' release '\'''/''\''];|g' $MATLABPATH/+IntegrationScripts/+common/communicatingSubmitFcn.m
+sed -i '50s|.*|cd '$workdir'|g' $MATLABPATH/+IntegrationScripts/+common/communicatingJobWrapper.sh
+sed -i '46s|.*|rjsl = ['\'''$workdir/Jobs/''\'' release];|g' $MATLABPATH/configCluster.m
+
